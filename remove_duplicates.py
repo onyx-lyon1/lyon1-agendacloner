0a3f65afc76b03b77a66603e4b6678ceb3f4cb64
eymeric <eymericdechelette@gmail.com>
2023-09-13 10:33:36 +0200

    adapte to new optimization

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -29,19 +29,21 @@ dirs = [
 def clean_duplicate(directories):
     if type(directories) is not list:
         copied = directories
-        copied['children'] = clean_duplicate(copy.deepcopy(copied['children']))
+        if copied['children'] is not None:
+            copied['children'] = clean_duplicate(copy.deepcopy(copied['children']))
     else:
         count = {}
         for directory in directories:
-            count[str(directory["id"])] = count.get(str(directory["id"]), 0) + 1
+            count[str(directory["identifier"])] = count.get(str(directory["identifier"]), 0) + 1
         copied = []
         for directory in directories:
-            if count[str(directory["id"])] > 1:
-                count[str(directory["id"])] -= 1
+            if count[str(directory["identifier"])] > 1:
+                count[str(directory["identifier"])] -= 1
             else:
                 copied.append(copy.deepcopy(directory))
         for item in copied:
-            item['children'] = clean_duplicate(copy.deepcopy(item['children']))
+            if item['children'] is not None:
+                item['children'] = clean_duplicate(copy.deepcopy(item['children']))
     return copied
 
 
